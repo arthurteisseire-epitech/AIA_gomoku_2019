2965938066979948780aa9d056f234a309797337
arthur <arthur.teisseire@epitech.eu>
2019-10-12 22:26:04 +0200

    Add commands in brain

--- pbrain-gomoku-ai.py
+++ pbrain-gomoku-ai.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python3
 
+import sys
+from random import randrange
 
+
+def send(s):
+    print(s)
+    sys.stdout.flush()
+
+
+def start():
+    send("OK")
+
+
+def turn():
+    send(str(randrange(19)) + ',' + str(randrange(19)))
+
+
+commands = [
+    ["START", start],
+    ["TURN", turn],
+    ["BEGIN", turn],
+]
+
+for line in sys.stdin:
+    for command in commands:
+        if command[0] in line:
+            command[1]()
