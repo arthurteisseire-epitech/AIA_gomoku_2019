0777fba7171331b1830c2e8931ee4d98682fa505
arthur <arthur.teisseire@epitech.eu>
2019-10-13 16:10:46 +0200

    Add Rules class

---
+++ Rules.py
@@ -0,0 +1,12 @@
+class Rules:
+    @staticmethod
+    def get_board_size():
+        return 19
+
+    @staticmethod
+    def get_timeout_turn_in_ms():
+        return 5000
+
+    @staticmethod
+    def get_max_memory_in_bytes():
+        return 70000000
--- tests/DispatcherTest.py
+++ tests/DispatcherTest.py
@@ -4,4 +4,7 @@ from Dispatcher import Dispatcher
 
 class DispatcherTest(TestCase):
     def test_start(self):
-        self.assertEqual("OK", Dispatcher.dispatch("START 20"))
+        self.assertEqual("OK", Dispatcher.dispatch("START 19"))
+
+    def test_restart(self):
+        self.assertEqual("OK", Dispatcher.dispatch("RESTART"))
