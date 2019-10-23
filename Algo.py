87575326b97bccfe2415a881c2f354b0cc135ac8
arthur <arthur.teisseire@epitech.eu>
2019-10-23 19:48:41 +0200

    Remove function argument in minimax and use evaluation function directly

--- Algo.py
+++ Algo.py
@@ -1,13 +1,13 @@
 from Weight import Weight
-from Board import *
-from Pos import *
+from Board import Tile
+from Pos import Pos
 from copy import deepcopy
 
 
 class Algo:
     @staticmethod
-    def minimax(board, evaluation_function, depth, maximizing_player=False):
-        e = evaluation_function(board)
+    def minimax(board, depth, maximizing_player=False):
+        e = Weight.evaluation_board(board)
         if e == Weight.LOOSE_GAME or e == Weight.WIN_GAME:
             return e
         if board.is_full():
@@ -21,7 +21,7 @@ class Algo:
                     if board.get_info_at(tmp_pos) == Tile.EMPTY:
                         new_board = deepcopy(board)
                         new_board.set_info_at(tmp_pos, Tile.MINE)
-                        tmp_eval = Algo.minimax(new_board, evaluation_function, depth - 1, False)
+                        tmp_eval = Algo.minimax(new_board, depth - 1, False)
                         max_eval = max(tmp_eval, max_eval)
             return max_eval
         else:
@@ -32,6 +32,6 @@ class Algo:
                     if board.get_info_at(tmp_pos) == Tile.EMPTY:
                         new_board = deepcopy(board)
                         new_board.set_info_at(tmp_pos, Tile.OPPONENT)
-                        tmp_eval = Algo.minimax(new_board, evaluation_function, depth - 1, True)
+                        tmp_eval = Algo.minimax(new_board, depth - 1, True)
                         min_eval = min(tmp_eval, min_eval)
             return min_eval
