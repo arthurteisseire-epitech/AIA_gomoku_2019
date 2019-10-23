fe9d27532f31fd6eabf76b953763899910b86ed6
arthur <arthur.teisseire@epitech.eu>
2019-10-23 19:37:13 +0200

    Rename evaluation_of_position into evaluation board and refactor it

--- Weight.py
+++ Weight.py
@@ -10,69 +10,37 @@ class Weight:
     INFINITE = math.inf
 
     @staticmethod
-    def evaluation_of_position(board: Board):
+    def evaluation_board(board: Board):
         for i in range(0, env.STONES_TO_WIN):
-            weight = Weight.__calc_weight_for(board.get_row_at(Pos(i, 0)))
-            if weight > 0:
-                return 10
-            elif weight < 0:
-                return -10
+            val = Weight.__calc_val_for(board.get_row_at(Pos(i, 0)))
+            if val != 0:
+                return val
 
         for i in range(0, env.STONES_TO_WIN):
-            weight = Weight.__calc_weight_for(board.get_col_at(Pos(0, i)))
-            if weight > 0:
-                return 10
-            elif weight < 0:
-                return -10
+            val = Weight.__calc_val_for(board.get_col_at(Pos(0, i)))
+            if val != 0:
+                return val
 
-        weight = Weight.__calc_weight_for(board.get_principal_diagonal(Pos(0, 0)))
-        if weight > 0:
-            return 10
-        elif weight < 0:
-            return -10
+        val = Weight.__calc_val_for(board.get_principal_diagonal(Pos(0, 0)))
+        if val != 0:
+            return val
 
-        weight = Weight.__calc_weight_for(board.get_counter_diagonal(Pos(0, 2)))
-        if weight > 0:
-            return 10
-        elif weight < 0:
-            return -10
+        val = Weight.__calc_val_for(board.get_counter_diagonal(Pos(0, 2)))
+        if val != 0:
+            return val
 
         return 0
 
     @staticmethod
-    def __calc_weight_for(array):
-        # if len(array) < env.STONES_TO_WIN:
-        #     return 0
-
-        my_nb_tiles = Weight.__count_same_tile_in(array, Tile.MINE)
-        if my_nb_tiles == 3:
+    def __calc_val_for(array):
+        my_consecutive_tiles = Weight.__count_same_tile_in(array, Tile.MINE)
+        if my_consecutive_tiles == 3:
             return Weight.WIN_GAME
-        opponent_nb_tiles = Weight.__count_same_tile_in(array, Tile.OPPONENT)
-        if opponent_nb_tiles == 3:
+        opponent_consecutive_tiles = Weight.__count_same_tile_in(array, Tile.OPPONENT)
+        if opponent_consecutive_tiles == 3:
             return Weight.LOOSE_GAME
         return 0
 
     @staticmethod
     def __count_same_tile_in(array, tile):
         return len(list(filter(lambda x: x == tile, array)))
-    #
-    # @staticmethod
-    # def __attribute_weight(my_weight, opponent_weight):
-    #     weights = [
-    #         Weight.__win_game(my_weight, opponent_weight),
-    #         Weight.__lose_game(my_weight, opponent_weight),
-    #     ]
-    #     return max(weights)
-    #
-    # @staticmethod
-    # def __win_game(my_weight, opponent_weight):
-    #     if my_weight == 3:
-    #         return Weight.WIN_GAME
-    #     return 0
-    #
-    # @staticmethod
-    # def __lose_game(my_weight, opponent_weight):
-    #     if opponent_weight == 3:
-    #         return Weight.LOOSE_GAME
-    #     return 0
-    #
--- tests/AITest.py
+++ tests/AITest.py
@@ -5,12 +5,22 @@ from AI import AI
 
 
 class AITest(TestCase):
-    def test_evaluation_of_best_position(self):
+    def test_find_best_position_one_stone(self):
         custom_board = Board(3, [
             [Tile.OPPONENT, Tile.EMPTY, Tile.EMPTY],
             [Tile.EMPTY, Tile.EMPTY, Tile.EMPTY],
             [Tile.EMPTY, Tile.EMPTY, Tile.EMPTY]
         ])
         expected = Pos(1, 1)
+        actual = AI.find_best_position(custom_board)
+        self.assertEqual(expected, actual, "\nexpected: " + expected.to_string() + "\ngot: " + actual.to_string())
+
+    def test_find_best_position_two_stone(self):
+        custom_board = Board(3, [
+            [Tile.OPPONENT, Tile.EMPTY, Tile.EMPTY],
+            [Tile.EMPTY, Tile.MINE, Tile.EMPTY],
+            [Tile.EMPTY, Tile.EMPTY, Tile.OPPONENT]
+        ])
+        expected = Pos(0, 1)
         actual = AI.find_best_position(custom_board)
         self.assertEqual(expected, actual, "\nexpected: " + expected.to_string() + "\ngot: " + actual.to_string())
