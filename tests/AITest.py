22b66f1d9e60ad5d258ebf2bd9bbfda330246d6c
arthur <arthur.teisseire@epitech.eu>
2019-10-23 19:26:12 +0200

    Refactor test of best position and remove evaluation tests

--- tests/AITest.py
+++ tests/AITest.py
@@ -2,33 +2,15 @@ from unittest import TestCase
 from Board import Board, Tile
 from Pos import *
 from AI import AI
-from Weight import Weight
 
 
 class AITest(TestCase):
-
-    def test_evaluation_of_position_error(self):
-        custom_board = Board(3, [
-            [Tile.EMPTY, Tile.EMPTY, Tile.OPPONENT],
-            [Tile.EMPTY, Tile.EMPTY, Tile.EMPTY],
-            [Tile.MINE, Tile.EMPTY, Tile.EMPTY]
-        ])
-        self.assertEqual(0, Weight.evaluation_of_position(custom_board, Pos(-1, 0)))
-
-    def test_evaluation_of_position(self):
-        custom_board = Board(3, [
-            [Tile.EMPTY, Tile.EMPTY, Tile.OPPONENT],
-            [Tile.EMPTY, Tile.EMPTY, Tile.EMPTY],
-            [Tile.MINE, Tile.EMPTY, Tile.MINE]
-        ])
-        self.assertEqual(8, Weight.evaluation_of_position(custom_board, Pos(0, 0)))
-
     def test_evaluation_of_best_position(self):
         custom_board = Board(3, [
-            [Tile.EMPTY, Tile.EMPTY, Tile.OPPONENT],
+            [Tile.OPPONENT, Tile.EMPTY, Tile.EMPTY],
             [Tile.EMPTY, Tile.EMPTY, Tile.EMPTY],
-            [Tile.MINE, Tile.EMPTY, Tile.MINE]
+            [Tile.EMPTY, Tile.EMPTY, Tile.EMPTY]
         ])
-        expected = Pos(2, 1)
-        actual = AI.find_best_positions(custom_board)[0].pos
+        expected = Pos(1, 1)
+        actual = AI.find_best_position(custom_board)
         self.assertEqual(expected, actual, "\nexpected: " + expected.to_string() + "\ngot: " + actual.to_string())
