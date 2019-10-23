8db3974cfcc162a009c0d2d351900f691803f3e6
arthur <arthur.teisseire@epitech.eu>
2019-10-23 19:23:37 +0200

    Rename weight into val in find_best_position

--- AI.py
+++ AI.py
@@ -17,12 +17,12 @@ class AI:
         for y in range(0, board.size):
             for x in range(0, board.size):
                 pos = Pos(y, x)
-                weight = -Weight.INFINITE
+                val = -Weight.INFINITE
                 if board.get_info_at(pos) == Tile.EMPTY:
                     new_board = deepcopy(board)
                     new_board.set_info_at(pos, Tile.MINE)
-                    weight = Algo.minimax(new_board, Weight.evaluation_of_position, 5)
-                if weight > max_val:
-                    max_val = weight
+                    val = Algo.minimax(new_board, Weight.evaluation_of_position, 5)
+                if val > max_val:
+                    max_val = val
                     best_pos = pos
         return best_pos
