7ae9e737526f634f2101206707df44100d6ef5b8
arthur <arthur.teisseire@epitech.eu>
2019-10-27 16:42:35 +0100

    Remove commentaries

--- ai/AI.py
+++ ai/AI.py
@@ -20,33 +20,8 @@ class AI:
             if board.get_info_at(pos) == Tile.EMPT:
                 new_board = deepcopy(board)
                 new_board.set_info_at(pos, Tile.MINE)
-                val = Algo.minimax(new_board, 0)
+                val = Algo.minimax(new_board, 1)
                 if val > max_val:
                     max_val = val
                     best_pos = pos
         return best_pos
-        #
-        # for y in range(0, board.size):
-        #     for x in range(0, board.size):
-        #         pos = Pos(y, x)
-        #         val = -Evaluation.INFINITE
-        #         if board.get_info_at(pos) == Tile.EMPT:
-        #             new_board = deepcopy(board)
-        #             new_board.set_info_at(pos, Tile.MINE)
-        #             val = Algo.minimax(new_board, 3)
-        #         if val > max_val:
-        #             max_val = val
-        #             best_pos = pos
-        # return best_pos
-
-    # @staticmethod
-    # def find_potential_positions(board):
-    #     best_positions = []
-    #     for y in range(0, board.size):
-    #         for x in range(0, board.size):
-    #             pos = Pos(y, x)
-    #             if board.get_info_at(pos) == Tile.EMPT:
-    #                 new_board = deepcopy(board)
-    #                 new_board.set_info_at(pos, Tile.MINE)
-    #                 best_positions.append([Evaluation.evaluation_board(new_board, Player.MINE), pos])
-    #     return sorted(best_positions, key=lambda elem: elem[0])[:5]
